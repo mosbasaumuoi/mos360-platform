f799f53aceaa3b914520437cd663061c09f3571c
mosbasaumuoi <mos360.vn@gmail.com>
2026-04-29 18:06:00 +0700

    Create home.js

---
+++ src/pages/home.js
@@ -0,0 +1,8 @@
+export function getHomePage() {
+  return `
+    <section style="padding:40px;text-align:center;">
+      <h1>MOS360</h1>
+      <p>Hệ sinh thái kỹ năng số cho sinh viên</p>
+    </section>
+  `;
+}

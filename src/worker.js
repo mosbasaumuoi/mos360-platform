444b87fe17e14f925d0b14a8f5b03e2387df8b35
mosbasaumuoi <mos360.vn@gmail.com>
2026-04-29 23:58:03 +0700

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,47 @@
+import { CONFIG } from "./config.js";
+import { getHomeUI } from "./pages/home.js";
+
 export default {
-  fetch() {
-    return new Response("OK");
+  async fetch(request, env) {
+
+    const url = new URL(request.url);
+    const path = url.pathname;
+
+    let content = "";
+
+    if (path === "/") {
+
+      content = getHomeUI("test");
+
+    } else {
+
+      content = "<h1>404</h1>";
+
+    }
+
+    return new Response(layout(content), {
+      headers: {
+        "Content-Type": "text/html;charset=UTF-8"
+      }
+    });
   }
+};
+
+function layout(content) {
+
+  return `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="UTF-8">
+    <title>${CONFIG.TITLE}</title>
+  </head>
+
+  <body>
+
+    ${content}
+
+  </body>
+  </html>
+  `;
 }

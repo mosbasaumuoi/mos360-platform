5e0145eabc3bcd11284af0b4c3408b9ca25b9d40
mosbasaumuoi <mos360.vn@gmail.com>
2026-04-29 22:19:11 +0700

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,9 +1,4 @@
-const CONFIG = {
-  TITLE: "MOS360 - Luyện thi MOS 1000/1000",
-  LOGO_URL: "https://raw.githubusercontent.com/mosbasaumuoi/mos360-web/main/logo%20vien.png",
-  SHEET_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vShTOF13wljdvKF0Olw_s3H4yTMZtlm0LE4Ui7CR-G2OoNQmvrMGUk67YZmoET84GcAV7nu_stXw2zV/pub?output=tsv"
-};
-
+import { CONFIG } from "./config/config.js";
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);

99623b6fff724b5e186b8b20f2c94123fd0ae5ed
mosbasaumuoi <mos360.vn@gmail.com>
2026-04-30 01:15:07 +0700

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,7 @@
 import { CONFIG } from "./config/config.js";
+import { layout } from "./layout.js";
 import { getHomeUI } from "./pages/home.js";
+import { getCoursesPage } from "./pages/courses.js";
 
 export default {
   async fetch(request) {
@@ -10,12 +12,18 @@ export default {
     let content = "";
 
     if (path === "/") {
-      content = getHomeUI("test");
-    } else {
-      content = "<h1>404</h1>";
+      content = getHomeUI();
     }
 
-    return new Response(layout(content, CONFIG), {
+    else if (path === "/courses") {
+      content = getCoursesPage();
+    }
+
+    else {
+      content = "<h1>404 - Không tìm thấy trang</h1>";
+    }
+
+    return new Response(layout(content), {
       headers: {
         "Content-Type": "text/html;charset=UTF-8"
       }

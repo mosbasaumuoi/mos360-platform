e92bf632f7a7724778243e956c6c103efd2a8587
mosbasaumuoi <mos360.vn@gmail.com>
2026-04-29 18:05:14 +0700

    Create config.js

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+export const CONFIG = {
+  TITLE: "MOS360",
+  DOMAIN: "https://mos360.vn",
+  LOGO_URL: "https://raw.githubusercontent.com/mosbasaumuoi/mos360-web/main/logo%20vien.png",
+
+  SHEET_URL:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vShTOF13wljdvKF0Olw_s3H4yTMZtlm0LE4Ui7CR-G2OoNQmvrMGUk67YZmoET84GcAV7nu_stXw2zV/pub?output=tsv"
+};

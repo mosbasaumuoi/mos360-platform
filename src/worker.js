d76cd4887a25a9db29e206a6e838f5ee27935676
mosbasaumuoi <mos360.vn@gmail.com>
2026-05-02 04:09:27 +0700

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,7 +1,24 @@
 import { router } from "./gateway/router.js";
+import { authMiddleware } from "./middleware/auth.middleware.js";
 
 export default {
   async fetch(request, env, ctx) {
+
+    // 🧭 STEP 7 - MIDDLEWARE CHAIN
+    const authResult = await authMiddleware(request, env);
+
+    // ❌ nếu fail auth thì chặn luôn
+    if (!authResult.ok) {
+      return new Response(JSON.stringify(authResult), {
+        status: 401,
+        headers: { "Content-Type": "application/json" }
+      });
+    }
+
+    // ✅ gắn user vào request
+    request.user = authResult.user;
+
+    // 🚀 đi tiếp vào API Gateway
     return router(request, env, ctx);
   }
 };

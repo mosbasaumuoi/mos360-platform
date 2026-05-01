bdf51655fae4482fbf2c970ddc016850e2a25b18
mosbasaumuoi <mos360.vn@gmail.com>
2026-05-02 01:27:10 +0700

    Delete utils directory

--- utils/response.js
+++
@@ -1,9 +0,0 @@
-export function json(data, status = 200) {
-  return new Response(JSON.stringify(data), {
-    status,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
-    }
-  });
-}

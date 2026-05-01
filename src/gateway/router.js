48ba173541c3abf29bc68319d84f018203767585
mosbasaumuoi <mos360.vn@gmail.com>
2026-05-02 02:15:15 +0700

    Update router.js

--- src/gateway/router.js
+++ src/gateway/router.js
@@ -1,23 +1,19 @@
-import { handleCourses } from "../modules/courses/courses.routes.js";
 import { handleAuth } from "../modules/auth/auth.routes.js";
+import { handleCourses } from "../modules/courses/courses.routes.js";
 import { handleAdmin } from "../modules/admin/admin.routes.js";
 
 export async function router(request, env, ctx) {
   const url = new URL(request.url);
-  const path = url.pathname;
 
-  // COURSES
-  if (path.startsWith("/api/courses")) {
-    return handleCourses(request, env, ctx);
-  }
-
-  // AUTH
-  if (path.startsWith("/api/auth")) {
+  if (url.pathname.startsWith("/api/auth")) {
     return handleAuth(request, env, ctx);
   }
 
-  // ADMIN
-  if (path.startsWith("/api/admin")) {
+  if (url.pathname.startsWith("/api/courses")) {
+    return handleCourses(request, env, ctx);
+  }
+
+  if (url.pathname.startsWith("/api/admin")) {
     return handleAdmin(request, env, ctx);
   }
 

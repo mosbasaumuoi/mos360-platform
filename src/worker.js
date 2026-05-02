e487e0e0879e2d0fdbc375f10b9796ba13f56dc8
mosbasaumuoi <mos360.vn@gmail.com>
2026-05-02 21:25:49 +0700

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -8,6 +8,20 @@ export default {
     // 🧭 STEP 7 - MIDDLEWARE CHAIN
     const authResult = await authMiddleware(request, env);
     const runtime = createRuntimeContext(env);
+        runtime.cache.set("test", "MOS360");
+
+    const value = runtime.cache.get("test");
+
+    runtime.events.on("student.login", (payload) => {
+      console.log("Student login:", payload.name);
+    });
+
+    runtime.events.emit("student.login", {
+      name: "MOS Student",
+    });
+
+    return new Response(value);
+    }
 
     // ❌ nếu fail auth thì chặn luôn
     if (!authResult.ok) {

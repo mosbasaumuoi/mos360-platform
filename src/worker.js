dd20dc919cd3ccb192c263eb5d1b545758d93d3d
mosbasaumuoi <mos360.vn@gmail.com>
2026-05-02 21:49:16 +0700

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -3,7 +3,7 @@ import { authMiddleware } from "./middleware/auth.middleware.js";
 import { createRuntimeContext } from "./runtime/runtimeContext.js";
 
 export default {
-  async fetch(request, env, ctx) {
+  async fetch(request, env, ctx, runtime) {
 
     // ⚡ Runtime Layer
     const runtime = createRuntimeContext(env);

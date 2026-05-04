982eccd2958a6c412e60a401a96c6b0aa2673c0e
mosbasaumuoi <mos360.vn@gmail.com>
2026-05-04 14:46:03 +0700

    Update tracking.service.js

--- src/services/tracking.service.js
+++ src/services/tracking.service.js
@@ -1,17 +1,10 @@
 export async function trackClick(runtime, source) {
 
-  const { env, cache } = runtime;
-
-  // ❗ không cache tracking
-  const current = await env.MOS360_TRACKING.get(source);
-  const count = current ? parseInt(current) : 0;
-
-  const newCount = count + 1;
-
-  await env.MOS360_TRACKING.put(source, String(newCount));
+  await runtime.events.emit("track.click", {
+    source
+  });
 
   return {
-    source,
-    count: newCount
+    source
   };
 }

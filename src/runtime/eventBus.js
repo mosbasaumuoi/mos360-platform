ad3e2d84488bb2e5dc08f3094a3786214e5aa73b
mosbasaumuoi <mos360.vn@gmail.com>
2026-05-04 14:58:59 +0700

    Update eventBus.js

--- src/runtime/eventBus.js
+++ src/runtime/eventBus.js
@@ -1,6 +1,7 @@
 const listeners = {};
 
 export const eventBus = {
+
   on(event, callback) {
     if (!listeners[event]) {
       listeners[event] = [];
@@ -9,16 +10,18 @@ export const eventBus = {
     listeners[event].push(callback);
   },
 
-async emit(event, payload) {
+  async emit(event, payload) {
 
-  const handlers = listeners[event] || [];
+    const handlers = listeners[event] || [];
 
-  for (const handler of handlers) {
-    try {
-      await handler(payload);
-    } catch (err) {
-      console.error("Event handler error:", err);
+    for (const handler of handlers) {
+      try {
+        await handler(payload);
+      } catch (err) {
+        console.error("Event handler error:", err);
+      }
     }
+
   }
 
-}
+};

d1fd300e11cad6eac7d347cdde422b69bfb5fb38
mosbasaumuoi <mos360.vn@gmail.com>
2026-05-05 23:59:09 +0700

    Update auth.service.js

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,44 +1,29 @@
 import { SignJWT, jwtVerify } from "jose";
 
-export async function login(email, password, env) {
+const encoder = new TextEncoder();
 
-  // demo user
+export async function login(email, password, env) {
+  // DEMO: hardcode 1 user admin
   if (email !== "admin@mos360.vn" || password !== "123456") {
     return null;
   }
 
-  const SECRET = new TextEncoder().encode(env.JWT_SECRET);
-
-  const token = await new SignJWT({
-    id: 1,
-    email,
-    role: "admin"
-  })
+  const token = await new SignJWT({ role: "admin", email })
     .setProtectedHeader({ alg: "HS256" })
-    .setIssuedAt()
     .setExpirationTime("2h")
-    .sign(SECRET);
+    .sign(encoder.encode(env.JWT_SECRET));
 
-  return {
-    token,
-    user: {
-      id: 1,
-      email,
-      role: "admin"
-    }
-  };
+  return { token };
 }
 
 export async function verifyToken(token, env) {
-
   try {
-    const SECRET = new TextEncoder().encode(env.JWT_SECRET);
-
-    const { payload } = await jwtVerify(token, SECRET);
-
+    const { payload } = await jwtVerify(
+      token,
+      encoder.encode(env.JWT_SECRET)
+    );
     return payload;
-
-  } catch (err) {
+  } catch {
     return null;
   }
 }

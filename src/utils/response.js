928a6011a4466f3a99a03f39b629c7e4e9bb211f
mosbasaumuoi <mos360.vn@gmail.com>
2026-05-05 13:30:44 +0700

    Update response.js

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,9 +1,31 @@
-export function json(data, status = 200) {
-  return new Response(JSON.stringify(data), {
-    status,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
+export function json(data = null, status = 200) {
+  return new Response(
+    JSON.stringify({
+      ok: status >= 200 && status < 300,
+      data,
+      error: status >= 400 ? data : null
+    }),
+    {
+      status,
+      headers: {
+        "Content-Type": "application/json"
+      }
     }
-  });
+  );
+}
+
+export function error(message = "Error", status = 500) {
+  return new Response(
+    JSON.stringify({
+      ok: false,
+      data: null,
+      error: message
+    }),
+    {
+      status,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
 }

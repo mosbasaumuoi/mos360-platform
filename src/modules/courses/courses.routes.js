b181ae3ed5a60333d5fd9704581a10e59fec1164
mosbasaumuoi <mos360.vn@gmail.com>
2026-05-06 00:32:39 +0700

    Update courses.routes.js

--- src/modules/courses/courses.routes.js
+++ src/modules/courses/courses.routes.js
@@ -1,7 +1,7 @@
 import { json } from "../../utils/response.js";
 
 export async function handleCourses(request, env) {
-  const list = await env.MOS360_DATA.get("courses");
+  const list = await env.MOS360_COURSES_KV.get("courses");
 
   return json(list ? JSON.parse(list) : []);
 }

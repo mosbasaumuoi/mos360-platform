c7912d8790f04007afd1e045325ea52fabd7bdc4
mosbasaumuoi <mos360.vn@gmail.com>
2026-05-05 09:32:53 +0700

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,9 @@
 import { router } from "./core/router.js";
+import { login, api } from "./core/apiClient.js";
+
+// expose để test trong console
+window.login = login;
+window.api = api;
 
 document.addEventListener("DOMContentLoaded", () => {
   router();

d006712218bcc63fd4e4ba636a320ec417b28715
mosbasaumuoi <mos360.vn@gmail.com>
2026-05-05 12:18:29 +0700

    Update auth.middleware.js

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -11,7 +11,7 @@ export async function authMiddleware(request, env) {
   try {
     const token = authHeader.split(" ")[1];
 
-    // 🔥 ĐẶT Ở ĐÂY (BÊN TRONG FUNCTION)
+    // ✅ env chỉ được dùng trong function
     const SECRET = new TextEncoder().encode(env.JWT_SECRET);
 
     const { payload } = await jwtVerify(token, SECRET);

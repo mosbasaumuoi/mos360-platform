0f90905921ac32e21e38ef6aabf49cd288cf9d26
mosbasaumuoi <mos360.vn@gmail.com>
2026-05-06 17:26:41 +0700

    Create footer.js

---
+++ frontend/components/footer/footer.js
@@ -0,0 +1,21 @@
+// ============================================
+// FOOTER COMPONENT
+// Footer dùng chung
+// ============================================
+
+export function renderFooter() {
+
+  return `
+    <footer class="footer">
+
+      <div>
+        MOS360 PLATFORM
+      </div>
+
+      <div>
+        Hải Phòng
+      </div>
+
+    </footer>
+  `;
+}
